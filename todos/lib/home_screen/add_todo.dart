995c2657c92968906a62ecc076a69053a7516f8b
legendry <youxiaobin@taihe.com>
2019-02-14 22:59:34 +0800

    ....

--- todos/lib/home_screen/add_todo.dart
+++ todos/lib/home_screen/add_todo.dart
@@ -3,17 +3,14 @@ import 'package:flutter/material.dart';
 class AddTodo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // title: "添加Todo",
-      home: Scaffold(
+    return Scaffold(
         appBar: AppBar(title: Text("添加Todo")),
         body: Column(
           children: <Widget>[
             // TextFormField(onSaved: (text) => print("输入的文本"))
             Text("............")
           ]
-        ),
-      ),
+        )
     );
   }
 }
